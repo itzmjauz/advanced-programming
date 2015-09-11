a9235a59b8b9b157a34000f97051cab13cb49567
itzmjauz <itzmjauz@gmail.com>
2015-09-11 16:26:04 +0200

    add identifier structure

---
+++ tim-structure-identifier.java
@@ -0,0 +1,17 @@
+package assignment1;
+
+public class Identifier {
+
+    private String name;
+
+    public Identifier(String name){
+        this.name = name;
+    }
+
+    /**
+     *  Returns the String name
+     */
+    public String getName(){
+
+    }    
+}
