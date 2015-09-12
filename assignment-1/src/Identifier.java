c73d8d1b71bcc35a6ffebc8bf38da4f3c6fc2824
itzmjauz <itzmjauz@gmail.com>
2015-09-12 15:02:31 +0200

    some extra stuff

--- assignment-1/src/Identifier.java
+++ assignment-1/src/Identifier.java
@@ -1,5 +1,18 @@
 package assignment1;
 
 interface Identifier {
-  public void get();
+  /**
+    * PRO :The constructor requires a valid non-empty string starting with a letter to be entered
+    * PRE : local sstring will be initiliazed
+    *
+    * PRE : No PRE
+    * PRO : will return a non-empty String
+    **/
+  public String get();
+  /**
+    * PRE : none
+    * OOST : resets the identifier to string
+    **/
+  public void init(String string);
+
 }
--- assignment-1/src/IdentifierSet.java
+++ assignment-1/src/IdentifierSet.java
@@ -1,6 +1,11 @@
 package assignment1;
 
 interface Set {
+  /**
+   * Set should not be empty
+   *
+   * Returns a new identifier for every iteration until the complete set has been returned, using identifierCounter
+   */
   public Identifier Get();
   public void AddIdentifier(Identifier identifier);
   public Set difference(Set set);
