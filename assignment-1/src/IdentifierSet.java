2bca6e51a215ac59db85978c7dc746e635427b7d
itzmjauz <itzmjauz@gmail.com>
2015-09-12 15:35:54 +0200

    move tims comments to the actual interface

--- assignment-1/src/IdentifierSet.java
+++ assignment-1/src/IdentifierSet.java
@@ -7,9 +7,34 @@ interface Set {
    * Returns a new identifier for every iteration until the complete set has been returned, using identifierCounter
    */
   public Identifier Get();
+  /**
+   * Takes as argument a valid identifier that meets the idintifier conditions
+   *
+   * Checks if the item to add is not a duplicate of items that are already in set
+   * If not, stores the identifier in the set
+   */
   public void AddIdentifier(Identifier identifier);
-  public Set difference(Set set);
-  public Set Union(Set set);
-  public Set SymmetricDifference(Set set);
+  /**
+   * Calling object and argument must be a valid set
+   * Returns a new IdentifierSet containing the elements in the 1st but not in the 2nd set
+   */
   public Set Intersection(Set set);
+
+
+  /**
+   * Calling object and argument must be a valid set
+   * Returns a new IdentifierSet containing the intersecting identifiers of the calling object and the argument of the method
+   * Omits duplicates
+   */
+  public Set Union(Set set);
+  /**
+   * Calling object and argument must be a valid set
+   * Returns a new IdentifierSet containing all elements that are not in both sets
+   */
+  public Set SymmetricDifference(Set set);
+  /**
+   * Calling object and argument must be a valid set
+   * Returns a new IdentifierSet containing the elements in the 1st but not in the 2nd set
+   */
+  public Set Difference(Set set);
 }
