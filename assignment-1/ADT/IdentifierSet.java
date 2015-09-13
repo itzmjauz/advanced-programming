9753898af7fe7f524ac5836521ca8e10e83aee29
Nedervino <tim.nederveen@hotmail.com>
2015-09-13 23:18:06 +0200

    Update IdentifierSet.java
    - Naar javadoc-layout gewijzigd en @pre/@postconditions aangemaakt
    - Set classname naar IdentifierSet gewijzigd
    - get() naar getIdentifier() gewijzigd
    - Exceptions toegevoegd
    
    TODO:
    - @domain moet nog bijgewerkt worden
    - checken voor consistentie

--- assignment-1/ADT/IdentifierSet.java
+++ assignment-1/ADT/IdentifierSet.java
@@ -1,54 +1,82 @@
 package assignment1;
 
-interface Set {
-  /**
-   * PRE :
-   * Set should not be empty
-   *
-   * POST :
-   * Returns a new identifier for every iteration until the complete set has been returned, using identifierCounter
-   */
-  public Identifier Get();
-  /**
-   * PRE :
-   * Takes as argument a valid identifier that meets the idintifier conditions
-   *
-   * POST :
-   * Checks if the item to add is not a duplicate of items that are already in set
-   * If not, stores the identifier in the set
-   */
-  public void AddIdentifier(Identifier identifier);
-  /**
-   * PRE :
-   * Calling object and argument must be a valid set
+/** ADT for the class IdentifierSet
+ *
+ * @author Antoni Stevenet
+ * @author Tim Nederveen
+ * @elements
+ *	identifiers of the type Identifier
+ * @structure 
+ *	non linear
+ * @domain
+ *	//wat moet hier?
+ * @constructor
+ *	IdentifierSet();
+ *	    <dl>
+ *		<dt><b>PRE-condition</b><dd> -
+ *		<dt><b>POST-condition</b><dd>The new created
+ *		IdentifierSet object is empty and can contain a maximum of 20 Identifiers
+ *	    </dl>
+ *	<br>
+ **/
+ 
+public interface IdentifierSet {
 
-   * POST :
-   * Returns a new IdentifierSet containing the elements in the 1st but not in the 2nd set
-   */
-  public Set Intersection(Set set);
-  /**
-   * PRE :
-   * Calling object and argument must be a valid set
-   *
-   * POST :
-   * Returns a new IdentifierSet containing the intersecting identifiers of the calling object and the argument of the method
-   * Omits duplicates
-   */
-  public Set Union(Set set);
-  /**
-   * PRE :
-   * Calling object and argument must be a valid set
-   *
-   * POST :
-   * Returns a new IdentifierSet containing all elements that are not in both sets
-   */
-  public Set SymmetricDifference(Set set);
-  /**
-   * PRE :
-   * Calling object and argument must be a valid set
-   *
-   * POST :
-   * Returns a new IdentifierSet containing the elements in the 1st but not in the 2nd set
-   */
-  public Set Difference(Set set);
+	/** Initializes the IdentifierSet object
+    * @precondition
+    *	    -
+    * @postcondition
+    *	    The new created IdentifierSet object is empty and can contain a maximum of 20 Identifiers
+    **/
+	public void init ();
+	
+	/** Returns an identifier from the set, can be iterated to return complete set
+	* @precondition
+	* 		Set should not be empty
+	* @postcondition
+	*		Returns a new identifier for every iteration until the complete set has been returned
+	*/
+	public Identifier getIdentifier();
+  
+	/** Adds an identifier to the set
+    * @precondition
+    *		Set should not be full
+    * @postcondition
+    * 		Checks if the item to add is not a duplicate of items that are already in set, if not it stores the identifier in the set
+    */
+    public void AddIdentifier(Identifier identifier);
+  
+    /** Returns a set with all identifiers which are in the 1st but not in the 2nd set
+    * @precondition
+    * 		-
+	* @postcondition
+    * 		Returns a new IdentifierSet containing the elements in the 1st but not in the 2nd set
+    */
+    public IdentifierSet Difference(IdentifierSet set);
+	
+    /** Returns a set with objects that are in both sets
+    * @precondition
+    * 		-
+    * @postcondition
+    * 		Returns a new IdentifierSet containing only the elements that are in both sets
+    */
+    public IdentifierSet Intersection(IdentifierSet set);
+  
+    /** Return a set with all identifiers that are in both sets 
+    * @precondition
+			-
+    * @postcondition
+	*		Throws an exception if number of elements in both sets combined(not taking duplicates into account) are bigger than maximum numbers of elements in set
+    * 		Returns a new IdentifierSet containing all elements of both sets combined, omits duplicates
+    */
+    public IdentifierSet Union(IdentifierSet set);
+  
+    /** Returns a set with all identifiers that are only in one of both sets
+    * @precondition
+	*		-
+    * @postcondition
+	*		Throws an exception if number of elements in resulting set exceeds maximum set size
+    * 		Returns a new IdentifierSet containing all elements that are only in one of both sets
+    */
+    public IdentifierSet SymmetricDifference(IdentifierSet set);
 }
