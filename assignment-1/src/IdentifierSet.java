54c4aa9ed7aa256986df13b15ceee8242f9b7ade
itzmjauz <itzmjauz@gmail.com>
2015-09-15 14:49:56 +0200

    untabify

--- assignment-1/src/IdentifierSet.java
+++ assignment-1/src/IdentifierSet.java
@@ -1,7 +1,7 @@
 package assignment1;
 
 public class IdentifierSet implements IdentifierSetADT {
-	
+
   private IdentifierADT[] set;
   private int size = 0;
   private int counter = 0;
@@ -19,15 +19,15 @@ public class IdentifierSet implements IdentifierSetADT {
   }
 
   public IdentifierADT getIdentifier() {
-	  if(counter == size()) counter = 0;
-	  	
-	  return set[counter++];
+    if(counter == size()) counter = 0;
+
+    return set[counter++];
   }
 
   public void AddIdentifier(IdentifierADT identifier) {
-  	set[size] = identifier;
-  	
-  	size++;
+    set[size] = identifier;
+
+    size++;
   }
 
   public boolean isEmpty() {
@@ -37,23 +37,23 @@ public class IdentifierSet implements IdentifierSetADT {
       return false;
     }
   }
-  
+
   public boolean isFull() {
-  	if(size() == MAX_SIZE) {
-  		return true;
-  	} else {
-  		return false;
-  	}
+    if(size() == MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  
+
   public IdentifierSetADT Difference(IdentifierSetADT secondSet) {
-  	for(int x = 0 ; x < size()) {
-  		
-  	}
+    for(int x = 0 ; x < size()) {
+
+    }
   }
-  
+
   public IdentifierSetADT Intersection(IdentifierSetADT set);
   public IdentifierSetADT Union(IdentifierSetADT set) throws Exception;
   public IdentifierSetADT SymmetricDifference(IdentifierSetADT set) throws Exception;
-  
+
 }
--- assignment-1/src/Identifiers.java
+++ assignment-1/src/Identifiers.java
@@ -22,55 +22,55 @@ public class Identifiers {
     IdentifierSetADT set1 = readSet(in);
     out.println("Give the second set: ");
     IdentifierSetADT set2 = readSet(in);
-    
+
     printResults(set1, set2);
   }
 
   IdentifierSetADT readSet(Scanner in) {
-  	IdentifierSetADT set = new IdentifierSet();
-  	
-  	while(in.hasNext()) {
-  		if(nextStringIsIdentifier(in)) {
-  			IdentifierADT identifier = new Identifier(in.next());
-  			set.AddIdentifier(identifier);
-  		}
-  	}
-  	
-  	return set;
+    IdentifierSetADT set = new IdentifierSet();
+
+    while(in.hasNext()) {
+      if(nextStringIsIdentifier(in)) {
+        IdentifierADT identifier = new Identifier(in.next());
+        set.AddIdentifier(identifier);
+      }
+    }
+
+    return set;
   }
-  
+
   boolean nextStringIsIdentifier(Scanner in) {
-  	return in.hasNext("^[a-zA-Z][a-zA-Z0-9]+");
+    return in.hasNext("^[a-zA-Z][a-zA-Z0-9]+");
   }
-  
+
   void printResults(IdentifierSetADT set1, IdentifierSetADT set2) {
-  	String result;
-  	// print string method not yet implemented but
-  	// assuming IdentifierSet.toString() prints the set in string format
-  	// handle exceptions later
-  	result = getSetString(set1.Difference(set2));
-  	out.println("Difference = " + result);
-  	result = getSetString(set1.Intersection(set2));
-  	out.println("Intersection = " + result);
-  	
-  	try {
-  	result = getSetString(set1.Union(set2));
-  	out.println("Union = " + result);
-  	result = getSetString(set1.SymmetricDifference(set2));
-  	out.println("Symmetric Difference = " + result);
-  	} 
-  	catch (Exception e){
-  		out.println(e);
-  	}
+    String result;
+    // print string method not yet implemented but
+    // assuming IdentifierSet.toString() prints the set in string format
+    // handle exceptions later
+    result = getSetString(set1.Difference(set2));
+    out.println("Difference = " + result);
+    result = getSetString(set1.Intersection(set2));
+    out.println("Intersection = " + result);
+
+    try {
+    result = getSetString(set1.Union(set2));
+    out.println("Union = " + result);
+    result = getSetString(set1.SymmetricDifference(set2));
+    out.println("Symmetric Difference = " + result);
+    }
+    catch (Exception e){
+      out.println(e);
+    }
   }
-  
+
   String getSetString(IdentifierSetADT set) {
-  	String string = "{";
-  	
-  	for(int i = 0 ; i < set.size() ; i++) {
-  		string = string + set.getIdentifier().getName() + " ";
-  	}
-  	
-  	return string + "}";
+    String string = "{";
+
+    for(int i = 0 ; i < set.size() ; i++) {
+      string = string + set.getIdentifier().getName() + " ";
+    }
+
+    return string + "}";
   }
 }
