0aae726b2cb3bfd58c680ffb3a3ca0509d8e9960
itzmjauz <itzmjauz@gmail.com>
2015-09-15 15:02:22 +0200

    untabify

--- assignment-1/src/IdentifierSet.java
+++ assignment-1/src/IdentifierSet.java
@@ -1,7 +1,7 @@
 package assignment1;
 
 public class IdentifierSet implements IdentifierSetADT {
-	
+
   private IdentifierADT[] set;
   private int size = 0;
   private int counter = 0;
@@ -19,15 +19,15 @@ public class IdentifierSet implements IdentifierSetADT {
   }
 
   public IdentifierADT getIdentifier() {
-	  if(counter == size()) counter = 0;
-	  	
-	  return set[counter++];
+    if(counter == size()) counter = 0;
+
+    return set[counter++];
   }
 
   public void AddIdentifier(IdentifierADT identifier) {
-  	set[size] = identifier;
-  	
-  	size++;
+    set[size] = identifier;
+
+    size++;
   }
 
   public boolean isEmpty() {
@@ -37,42 +37,42 @@ public class IdentifierSet implements IdentifierSetADT {
       return false;
     }
   }
-  
+
   public boolean isFull() {
-  	if(size() == MAX_SIZE) {
-  		return true;
-  	} else {
-  		return false;
-  	}
+    if(size() == MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  
+
   public IdentifierSetADT Difference(IdentifierSetADT set2) {
-  	IdentifierADT identifier1, identifier2;
-  	IdentifierSetADT result = new IdentifierSet();
-  	boolean contains;
-  	
-  	for(int x = 0 ; x < size() ; x++) {
-  		identifier1 = set[x];
-  		contains = false;
-  		
-  		for(int y = 0 ; y < set2.size() ; y++) {
-  			identifier2 = set2.getIdentifier();
-  			
-    		if(identifier1.equals(identifier2)) {
-    			contains = true;
-    		}
-  		}
-  		
-  		if(!contains) {
-  			result.AddIdentifier(identifier1);
-  		}
-  	}
-  	
-  	return result;
+    IdentifierADT identifier1, identifier2;
+    IdentifierSetADT result = new IdentifierSet();
+    boolean contains;
+
+    for(int x = 0 ; x < size() ; x++) {
+      identifier1 = set[x];
+      contains = false;
+
+      for(int y = 0 ; y < set2.size() ; y++) {
+        identifier2 = set2.getIdentifier();
+
+        if(identifier1.equals(identifier2)) {
+          contains = true;
+        }
+      }
+
+      if(!contains) {
+        result.AddIdentifier(identifier1);
+      }
+    }
+
+    return result;
   }
-  
+
   public IdentifierSetADT Intersection(IdentifierSetADT set);
   public IdentifierSetADT Union(IdentifierSetADT set) throws Exception;
   public IdentifierSetADT SymmetricDifference(IdentifierSetADT set) throws Exception;
-  
+
 }
