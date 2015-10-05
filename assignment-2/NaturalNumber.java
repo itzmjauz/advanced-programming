dfbc15398a347ff07527600efb0edfd5607afbe9
Antoni Stevenet <Itzmjauz@gmail.com>
2015-10-05 14:44:24 +0200

    add naturalnumber class

---
+++ assignment-2/NaturalNumber.java
@@ -0,0 +1,30 @@
+package assignment2;
+
+public class NaturalNumber implements NaturalNumberInterface {
+
+  private String number;
+
+  NaturalNumber (String number) {
+    this.number = number;
+  }
+
+  NaturalNumber (NaturalNumberInterface source) {
+    this.number = source.number();
+  }
+
+  public void init(String number) {
+    this.number = number;
+  }
+
+  public String number() {
+    return number;
+  }
+
+  public boolean equals(NaturalNumberInterface source) {
+    if(number.equals(source.number())) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
