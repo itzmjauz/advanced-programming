e03c4000d2cf53aba4611a23b74b93e54fece421
Antoni Stevenet <Itzmjauz@gmail.com>
2015-10-05 20:32:51 +0200

    update , add list ,data clonable

---
+++ assignment-2/Map.java
@@ -0,0 +1,56 @@
+package assignment2;
+
+public class MapInterface <K extends Data<K>, V extends Clonable<V>> extends Clonable<MapInterface> {
+
+  public void addKVPair(K key, V value) {
+    Wrapper entry = new Wrapper(key, value);
+  }
+
+  public boolean containsKey(K source) {
+    
+  }
+
+  public V returnValue (K source) {
+
+  }
+
+  public void removeForKey() {
+
+  }
+
+  priate class Wrapper <K extends Data, V extends Clonable> implements Data {
+    K key;
+    V value;
+
+    Wrapper() {
+      key = null;
+      value = null
+    }
+
+    Wrapper(K key, V value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    Wrapper(Wrapper source) {
+      key = source.key;
+      value = source.value;
+    }
+
+    void setKey(K key) {
+      this.key = key;
+    }
+
+    K getKey() {
+      return key;
+    }
+
+    void setValue(V value) {
+      this.value = value;
+    }
+
+    boolean equals(V value) {
+      return this.value == value; // or use .equals method ?
+    }
+  }
+}
