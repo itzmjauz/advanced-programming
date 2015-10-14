bd06d6814becf8be55b3256c0596d2facd895c72
Nedervino <tim.nederveen@hotmail.com>
2015-10-14 15:54:00 +0200

    Create LinkedListMethods.java
    Methods voor het werken met singly/doubly linked lists

---
+++ assignment-2/LinkedListMethods.java
@@ -0,0 +1,32 @@
+
+//aantal elementen in een singly linked list
+int numberOfElements(Node l) {
+    if (l == null) {
+        return 0;
+    }
+    return 1 + numberOfElements(l.next);
+}
+
+//recursief vinden of een waarde in een lijst zit
+boolean find (Node l, int x) {
+   if (l == null) {
+       return false;
+   }
+   return x == l.data || find(l.next, x);     //deze volgorde is belangrijk, de efficientste!
+}
+
+/*Onze lijst heeft een DOUBLY linked list, waarbij elke nieuwe entry gelijk op de juiste plek er gesorteerd
+ *ingezet moet worden, dus niet achteraf sorteren.
+ * Elke non-empty list heeft een reference naar het huidige element (current node)
+ */
+ 
+ //Nieuwe Node inserten tussen n en n.next, die dus terug verwijst naar n en vooruit verwijst
+ //naar n.next
+ new Node(5,n,n.next)
+ 
+ n.next = next.prior = new Node(5,n,n.next);      //verwijzingen van zowel n als n.next updaten
+ 
+ 
+
+
+
